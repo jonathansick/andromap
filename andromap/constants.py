f01191f38752d5175fe52cabe1f7a3e5e092192d
Jonathan Sick <jonathansick@mac.com>
2013-11-28 23:48:23 -0500

    Add constants module for RA, Dec distance to M31

---
+++ andromap/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Constants for Andromap
+
+2013-11-28 - Created by Jonathan Sick
+"""
+M31RA0 = 10.6846833
+M31DEC0 = 41.2690361
+
+D_KPC = 785  # distance to M31 in kpc
