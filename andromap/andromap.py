c8e2713b8782a8502d2fc88c430d2e91932ccf76
Jonathan Sick <jonathansick@mac.com>
2014-06-04 12:06:59 -0400

    Transform polygons to eq

--- andromap/andromap.py
+++ andromap/andromap.py
@@ -173,7 +173,9 @@ class Andromap(object):
             r_deg = np.arctan(r_kpc / D_KPC) * 180. / np.pi
             b_deg = (1. - ell) * r_deg  # semi-minor axis
             print r_kpc, r_deg, b_deg, ell, pa
-            poly = ellipse_polygon(r_deg, b_deg, pa, M31RA0, M31DEC0)
+            pa = pa - 90.  # make PA CCW from north
+            poly = ellipse_polygon(r_deg, b_deg, pa, M31RA0, M31DEC0,
+                                   n_verts=10)
             polygons.append(poly)
 
         self._f.show_polygons(polygons, layer=layer, zorder=zorder, **mpl)
@@ -204,5 +206,6 @@ def ellipse_polygon(r_deg, b_deg, pa, ra0, dec0, n_verts=1000):
     X = r_deg * np.cos(t) * np.cos(p) - b_deg * np.sin(t) * np.sin(p)
     Y = r_deg * np.cos(t) * np.sin(p) - b_deg * np.sin(t) * np.cos(p)
     ras, decs = tan_to_eq(X, Y, ra0Deg=ra0, dec0Deg=dec0)
-    verts = np.vstack([X, Y]).T
+    verts = np.vstack([ras, decs]).T
+    print verts
     return verts
